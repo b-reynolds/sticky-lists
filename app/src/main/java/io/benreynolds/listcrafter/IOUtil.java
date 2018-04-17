bfaf936940eba81602fc257689fd298aec7a9508
Ben Reynolds <breynolds1994@outlook.com>
2018-04-18 00:11:18 +0100

    Implement IOUtil class that provides static methods for the internal saving of 'ListEntry's

---
+++ app/src/main/java/io/benreynolds/listcrafter/IOUtil.java
@@ -0,0 +1,50 @@
+package io.benreynolds.listcrafter;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class IOUtil {
+
+    private static final String FILE_LIST_ENTRIES = "list_entries.ser";
+
+    public static boolean saveListEntries(final Context context, final ArrayList<ListEntry> listEntries) {
+        try {
+            FileOutputStream fileOutputStream = context.openFileOutput(FILE_LIST_ENTRIES, Context.MODE_PRIVATE);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            objectOutputStream.writeObject(listEntries);
+            objectOutputStream.close();
+
+            fileOutputStream.close();
+        }
+        catch(IOException exception) {
+            exception.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ArrayList<ListEntry> loadListEntries(final Context context) {
+        try {
+            FileInputStream fileInputStream = context.openFileInput(FILE_LIST_ENTRIES);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            ArrayList<ListEntry> listEntries = (ArrayList<ListEntry>) objectInputStream.readObject();
+            objectInputStream.close();
+            fileInputStream.close();
+            return listEntries;
+        }
+        catch(IOException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+}
