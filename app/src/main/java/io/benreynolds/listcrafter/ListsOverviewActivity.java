bda9473127a56388d7e2b4966d497157fed36442
Ben Reynolds <breynolds1994@outlook.com>
2018-04-17 11:01:58 +0100

    Create action bar button for adding new lists

--- app/src/main/java/io/benreynolds/listcrafter/ListsOverviewActivity.java
+++ app/src/main/java/io/benreynolds/listcrafter/ListsOverviewActivity.java
@@ -2,6 +2,8 @@ package io.benreynolds.listcrafter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ListsOverviewActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class ListsOverviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists_overview);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_lists_overview, menu);
+        return true;
+    }
+
 }
