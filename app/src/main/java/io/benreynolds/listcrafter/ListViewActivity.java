14bf75afe324ecc20945a28f18359a2172589fd7
Ben Reynolds <breynolds1994@outlook.com>
2018-04-19 02:36:04 +0100

    Add context menu to ListViewActivity for re-arranging/renaming/deleting list items

--- app/src/main/java/io/benreynolds/listcrafter/ListViewActivity.java
+++ app/src/main/java/io/benreynolds/listcrafter/ListViewActivity.java
@@ -6,14 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.InputType;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ListViewActivity extends AppCompatActivity {
 
@@ -33,6 +38,7 @@ public class ListViewActivity extends AppCompatActivity {
         mListItemListAdapter = new ListItemListAdapter(this, mListItems);
         lvListItems = findViewById(R.id.lvListItems);
         lvListItems.setAdapter(mListItemListAdapter);
+        registerForContextMenu(lvListItems);
     }
 
     @Override
@@ -53,6 +59,43 @@ public class ListViewActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, view, menuInfo);
+        if (view.getId()==R.id.lvListItems) {
+            getMenuInflater().inflate(R.menu.menu_list_item_long_press, menu);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        ListItem selectedListItem = (ListItem)lvListItems.getItemAtPosition(info.position);
+        switch(item.getItemId()) {
+            case R.id.move_up:
+                moveListItemUp(selectedListItem);
+                return true;
+            case R.id.move_down:
+                moveListItemDown(selectedListItem);
+                return true;
+            case R.id.send_top:
+                sendListItemToTop(selectedListItem);
+                return true;
+            case R.id.send_bottom:
+                sendListItemToBottom(selectedListItem);
+                return true;
+            case R.id.rename:
+                promptUserToRenameListItem(selectedListItem);
+                return true;
+            case R.id.delete:
+                deleteListItem(selectedListItem);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onPause() {
         IOUtil.saveListEntries(this, mListEntries);
         super.onPause();
@@ -60,8 +103,9 @@ public class ListViewActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        // Clear the currently stored 'ListEntry's.
+        // Clear the currently stored 'ListEntry's and 'ListItem's.
         mListEntries.clear();
+        mListItems.clear();
 
         // Load in the saved 'ListEntry's.
         ArrayList<ListEntry> savedListEntries = IOUtil.loadListEntries(this);
@@ -74,13 +118,142 @@ public class ListViewActivity extends AppCompatActivity {
         mListEntry = mListEntries.get(activeListItemIndex);
 
         // Clear and repopulate the active 'ListEntry's 'ListItem's.
-        mListItems.clear();
         mListItems.addAll(mListEntry.getListItems());
         mListItemListAdapter.notifyDataSetChanged();
 
         super.onResume();
     }
 
+    /**
+     * Moves the specified {@code ListItem} up one position in the list.
+     * @param listItem {@code ListItem} to move.
+     */
+    private void moveListItemUp(ListItem listItem) {
+        int listItemIndex = mListItems.indexOf(listItem);
+        if(listItemIndex != 0) {
+            Collections.swap(mListItems, listItemIndex, listItemIndex - 1);
+            lvListItems.setAdapter(mListItemListAdapter);
+            mListItemListAdapter.notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * Moves the specified {@code ListItem} down one position in the list.
+     * @param listItem {@code ListItem} to move.
+     */
+    private void moveListItemDown(ListItem listItem) {
+        int listItemIndex = mListItems.indexOf(listItem);
+        if(listItemIndex != mListItems.size() - 1) {
+            Collections.swap(mListItems, listItemIndex, listItemIndex + 1);
+            lvListItems.setAdapter(mListItemListAdapter);
+            mListItemListAdapter.notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * Moves the specified {@code ListItem} to the top of the list.
+     * @param listItem {@code ListItem} to move.
+     */
+    private void sendListItemToTop(ListItem listItem) {
+        int listItemIndex = mListItems.indexOf(listItem);
+        if(listItemIndex != 0) {
+            mListItems.add(0, mListItems.remove(listItemIndex));
+            lvListItems.setAdapter(mListItemListAdapter);
+            mListItemListAdapter.notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * Moves the specified {@code ListItem} to the bottom of the list.
+     * @param listItem {@code ListItem} to move.
+     */
+    private void sendListItemToBottom(ListItem listItem) {
+        int listItemIndex = mListItems.indexOf(listItem);
+        if(listItemIndex != mListItems.size() - 1) {
+            mListItems.add(mListItems.size() - 1, mListItems.remove(listItemIndex));
+            lvListItems.setAdapter(mListItemListAdapter);
+            mListItemListAdapter.notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * Renames the specified {@code ListItem}.
+     * @param listItem {@code ListItem} to rename.
+     * @param newDescription new description.                           
+     */
+    private void renameListItem(ListItem listItem, final String newDescription) {
+        if(newDescription.length() < ListItem.DESCRIPTION_LENGTH_MIN_CHARACTERS) {
+            Toast.makeText(this, String.format(getString(R.string.toast_rename_too_short), ListItem.DESCRIPTION_LENGTH_MIN_CHARACTERS), Toast.LENGTH_SHORT).show();
+        }
+        else if(newDescription.length() > ListItem.DESCRIPTION_LENGTH_MAX_CHARACTERS) {
+            Toast.makeText(this, String.format(getString(R.string.toast_rename_too_long), ListItem.DESCRIPTION_LENGTH_MIN_CHARACTERS), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            listItem.setDescription(newDescription);
+            mListItemListAdapter.notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * Deletes the specified {@code ListItem}.
+     * @param listItem {@code ListItem} to delete.
+     */
+    private void deleteListItem(ListItem listItem) {
+        mListItems.remove(listItem);
+        lvListItems.setAdapter(mListItemListAdapter);
+        mListItemListAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Prompts the user with a {@code DialogBox} to rename the specified {@code ListItem}.
+     * @param listItem {@code ListItem} to rename.
+     */
+    private void promptUserToRenameListItem(final ListItem listItem) {
+        // Create an AlertDialog.Builder that will be used to prompt users to enter a new description for the ListItem.
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        // Create a frame layout with an increased left and right margin so that the EditText box will look nicer.
+        FrameLayout frameLayout = new FrameLayout(this);
+        FrameLayout.LayoutParams layoutParams = new  FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+        layoutParams.rightMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+
+        // Create the EditText that will be used by users to enter their desired item description,
+        final EditText etItemName = new EditText(this);
+
+        // Assign the previously defined layout parameters to the EditText and apply properties that aim to restrict invalid data entry.
+        etItemName.setLayoutParams(layoutParams);
+        etItemName.setInputType(InputType.TYPE_CLASS_TEXT);
+        etItemName.setFilters( new InputFilter[] { new InputFilter.LengthFilter(ListItem.DESCRIPTION_LENGTH_MAX_CHARACTERS) } );
+        etItemName.setSingleLine();
+
+        // Add the EditText to the FrameLayout, and assign the FrameLayout to the 'DialogBox's view.
+        frameLayout.addView(etItemName);
+        alertDialogBuilder.setView(frameLayout);
+
+        // Set the title text of the DialogBox.
+        alertDialogBuilder.setTitle(R.string.dialog_title_list_rename);
+
+        // Add a positive button to the DialogBox that changes the description of the ListItem.
+        alertDialogBuilder.setPositiveButton(R.string.dialog_rename_item_positive_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                renameListItem(listItem, etItemName.getText().toString());
+            }
+        });
+
+        // Add a negative button to the DialogBox that closes the DialogBox and does change the description of the ListItem.
+        alertDialogBuilder.setNegativeButton(R.string.dialog_rename_item_negative_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Create and show the DialogBox.
+        alertDialogBuilder.create().show();
+    }
+    
     /**
      * Prompts the user with a {@code DialogBox} to input an item description and creates a new {@code ListItem}.
      */
